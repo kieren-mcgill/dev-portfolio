04f3b66627389adff25fb3cb6c7eb7981a8fd94c
kacm79 <105650651+kacm79@users.noreply.github.com>
2025-05-07 07:14:03 +0100

    global button compoenent created

---
+++ app/components/global/Button.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import {ReactNode} from "react";
+
+type ButtonProps = {
+    type: 'link' | 'ext-link' | 'submit'
+    path?: string
+    children: ReactNode
+}
+
+const Button = ({type, path, children}: ButtonProps) => {
+
+    const styles = `
+        uppercase font-bold border-b-2 border-green pb-2
+        hover:border-white
+        transition duration-300
+    `;
+
+    switch (type) {
+        case 'link':
+            return (
+                <Link
+                    className={styles}
+                    href={path || ""}
+                >
+                    {children}
+                </Link>
+            );
+        case 'ext-link':
+            return (
+                <a
+                    className={styles}
+                    href={path || ""}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {children}
+                </a>
+            );
+        case 'submit':
+            return (
+                <button
+                className={styles}
+                type="submit"
+                >
+                    {children}
+                </button>
+            )
+    }
+}
+
+export default Button;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Section from "@/app/components/global/Section";
+import Button from "@/app/components/global/Button";
 
 export default function Home() {
   return (
       <Section>
           <h1>Hello</h1>
+          <Button type={'link'}>A button</Button>
       </Section>
 
   );
